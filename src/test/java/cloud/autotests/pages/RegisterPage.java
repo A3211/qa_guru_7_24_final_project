0a87afc5c46e84989735f56fa5be9d075147e49c
A3211 <a.kusl1122@gmail.com>
2021-10-26 21:02:34 +0700

    added AttachmentHelper and TestBase

--- src/test/java/cloud/autotests/pages/RegisterPage.java
+++ src/test/java/cloud/autotests/pages/RegisterPage.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.selector.ByText;
 import com.github.javafaker.Faker;
 import com.github.javafaker.Name;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -28,42 +29,50 @@ public class RegisterPage {
 
     private SelenideElement myAccount = $(".header-links").$(".account");
 
+    @Step("Открываем страницу регистрации")
     public RegisterPage openPage() {
         open("http://demowebshop.tricentis.com/register");
         $(".page-title").shouldHave(Condition.text("Register"));
         return this;
     }
 
+    @Step("Вводим имя")
     public RegisterPage typeFirstName() {
         firstName.setValue(userName);
         return this;
     }
 
+    @Step("Вводим фамилию")
     public RegisterPage typeLastName() {
         lastName.setValue(userLastName);
         return this;
     }
 
+    @Step("Вводим емаил")
     public RegisterPage typeEmail() {
         email.setValue(userEmail);
         return this;
     }
 
+    @Step("Вводим пароль")
     public RegisterPage typePassword() {
         password.setValue(userPassword);
         return this;
     }
 
+    @Step("Вводим подтверждение пароля")
     public RegisterPage typeConfirmPassword() {
         confirmPassword.setValue(userPassword);
         return this;
     }
 
+    @Step("Жмем на кнопку [Register]")
     public RegisterPage clickRegisterButton() {
         registerButton.click();
         return this;
     }
 
+    @Step("Переходим в личный кабинет созданного пользователя, проверяем, что Имя, Фамилия, Емаил верны")
     public RegisterPage checkSuccessfulRegistration() {
         resultPage.shouldHave(Condition.text("Your registration completed"));
         myAccount.click();
